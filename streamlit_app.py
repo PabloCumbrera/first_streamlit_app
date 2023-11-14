c0d7ee85ec9e5058f80ec8dbe33ff1170bee3325
PabloCumbrera <76842771+PabloCumbrera@users.noreply.github.com>
2023-11-14 12:23:38 +0100

    Update streamlit_app.py
    Add data

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
+# Imports
 import streamlit
+import pandas
 
+
+# Headers
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
@@ -8,5 +12,12 @@ streamlit.text('🥣 🥗 🐔 🥑🍞')
 
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+# Add DATA
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+#Show
+streamlit.dataframe(my_fruit_list)
+
 
 
