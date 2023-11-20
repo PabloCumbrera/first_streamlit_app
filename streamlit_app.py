5208238fe93a1725412864aa7d49f3e4fb3a8b9d
PabloCumbrera <76842771+PabloCumbrera@users.noreply.github.com>
2023-11-20 10:45:26 +0100

    Update streamlit_app.py
    snowflake testing

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,9 +52,9 @@ streamlit.dataframe(fruityvice_normalized)
 # Snowflake part
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_cur.execute("SELECT * from fruit_load_list")
 my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
+streamlit.text("The fruit load cointais:")
 streamlit.text(my_data_row)
 
 
